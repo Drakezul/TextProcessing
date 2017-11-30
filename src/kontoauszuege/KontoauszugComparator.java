6f38cf723721c305a6c266646a250eb4e8a7017f
Drakezul <sebastian.grueb@web.de>
2017-11-30 20:19:35 +0100

    Updates to Git

--- src/kontoauszuege/KontoauszugComparator.java
+++ src/kontoauszuege/KontoauszugComparator.java
@@ -4,12 +4,30 @@ import java.io.File;
 import java.util.Comparator;
 
 public class KontoauszugComparator implements Comparator<File> {
-	
+
 	@Override
 	public int compare(File arg0, File arg1) {
 		if (!arg0.getPath().equals(arg1.getPath())) {
-			int dateArg0 = Integer.parseInt(arg0.getName().substring(28, 32) + arg0.getName().substring(25, 28));
-			int dateArg1 = Integer.parseInt(arg1.getName().substring(28, 32) + arg1.getName().substring(25, 28));
+			short v1YearStart = 28;
+			short v1NumberStart = 25;
+			short v2YearStart = 9;
+			short v2NumberStart = 17;
+			int dateArg0;
+			if (arg0.getName().startsWith("Kontoauszug")) {
+				dateArg0 = Integer.parseInt(arg0.getName().substring(v1YearStart, v1YearStart + 4)
+						+ arg0.getName().substring(v1NumberStart, v1NumberStart + 3));
+			} else {
+				dateArg0 = Integer.parseInt(arg0.getName().substring(v2YearStart, v2YearStart + 4)
+						+ arg0.getName().substring(v2NumberStart, v2NumberStart + 3));
+			}
+			int dateArg1;
+			if (arg1.getName().startsWith("Kontoauszug")) {
+				dateArg1 = Integer.parseInt(arg1.getName().substring(v1YearStart, v1YearStart + 4)
+						+ arg1.getName().substring(v1NumberStart, v1NumberStart + 3));
+			} else {
+				dateArg1 = Integer.parseInt(arg1.getName().substring(v2YearStart, v2YearStart + 4)
+						+ arg1.getName().substring(v2NumberStart, v2NumberStart + 3));
+			}
 			return dateArg0 - dateArg1;
 		} else {
 			return 0;
--- src/kontoauszuege/KontoauszugConverterToCSV.java
+++ src/kontoauszuege/KontoauszugConverterToCSV.java
@@ -2,9 +2,11 @@ package kontoauszuege;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
-
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -14,26 +16,41 @@ public class KontoauszugConverterToCSV implements Runnable {
 	private String filepath;
 	private ArrayList<String[]> results;
 
+	private static int setLength = 7;
+	// start at 1, skip header and first line
+
 	public static void main(String[] args) {
-		File resourcesFolder = new File(args[0]);		
+		File resourcesFolder = new File(args[0]);
 		File[] listOfFiles = resourcesFolder.listFiles(new FileFilter() {
 			@Override
 			public boolean accept(File file) {
 				return file.isFile() && file.getPath().endsWith(".txt");
 			}
 		});
-
 		Arrays.sort(listOfFiles, new KontoauszugComparator());
 
 		ArrayList<KontoauszugConverterToCSV> fileProcessor = new ArrayList<KontoauszugConverterToCSV>();
 		ArrayList<Thread> threads = new ArrayList<Thread>();
 
 		for (int i = 0; i < listOfFiles.length; i++) {
-			fileProcessor.add(new KontoauszugConverterToCSV(listOfFiles[i].getPath()));
-			threads.add(new Thread(fileProcessor.get(i)));
+			KontoauszugConverterToCSV temp = new KontoauszugConverterToCSV(listOfFiles[i].getPath());
+			fileProcessor.add(temp);
+			threads.add(new Thread(temp));
 		}
 		try {
-			FileWriter writer = new FileWriter(args[0] + "/Zusammenfassung.csv");
+			OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(args[0] + "/Zusammenfassung.csv"),
+					Charset.forName("Windows-1252").newEncoder());
+			String csvHeader = "Bu-Tag;Wert;Vorgang;Sender/Empfänger;Additionals;IBAN;Betrag;";
+			String[] anticipatedColumns = csvHeader.split(";");
+			System.out.println("Set length: " + setLength);
+			System.out.println("Anticipated sets: ");
+			for (String column : anticipatedColumns) {
+				System.out.println(column);
+			}
+			writer.append(csvHeader + "\n");
+
+			double sum = 0;
+			DecimalFormat centsFormat = new DecimalFormat("0.00");
 			for (int i = 0; i < fileProcessor.size(); i++) {
 				try {
 					threads.get(i).join();
@@ -41,10 +58,25 @@ public class KontoauszugConverterToCSV implements Runnable {
 					e.printStackTrace();
 				}
 				for (String[] currentSet : fileProcessor.get(i).results) {
-					for (String currentColumn : currentSet) {
+					for (int j = 0; j < setLength; j++) {
+						String currentColumn = currentSet[j];
+						if (currentColumn == null) {
+							currentColumn = "";
+						}
 						writer.append(currentColumn + ";");
 					}
-					writer.append('\n');
+					double deltaValue = 0;
+					// remove remove H, remove S, blank, transform to english
+					// decimal
+					String value = currentSet[setLength - 1].replaceAll("H", "").replaceAll("S", "").replaceAll(" ", "")
+							.replaceAll("\\.", "").replaceAll(",", ".");
+					if (currentSet[setLength - 1].endsWith("H")) {
+						deltaValue = Double.parseDouble(value);
+					} else if (currentSet[setLength - 1].endsWith("S")) {
+						deltaValue = (-1) * Double.parseDouble(value);
+					}
+					sum += deltaValue;
+					writer.append(centsFormat.format(sum) + ";\n");
 				}
 			}
 			writer.close();
@@ -62,9 +94,13 @@ public class KontoauszugConverterToCSV implements Runnable {
 		results = new ArrayList<String[]>();
 		File file = new File(filepath);
 		try {
-			Scanner fileScanner = new Scanner(file);
+			Scanner fileScanner = new Scanner(file, "UTF-8");
 			String currentLine = "";
+			String yearOfStatement = "";
 			while (!currentLine.contains("alter Kontostand")) {
+				if (currentLine.contains("erstellt am")) {
+					yearOfStatement = currentLine.split(" ")[2].split("\\.")[2];
+				}
 				currentLine = fileScanner.nextLine().trim().replaceAll("\\s+", " ");
 				while (currentLine.length() == 0) {
 					currentLine = fileScanner.nextLine().trim().replaceAll("\\s+", " ");
@@ -74,7 +110,7 @@ public class KontoauszugConverterToCSV implements Runnable {
 			if (currentLine.length() == 0) {
 				currentLine = fileScanner.nextLine().trim().replaceAll("\\s+", " ");
 			}
-			processSets(fileScanner, currentLine);
+			processSets(fileScanner, currentLine, yearOfStatement);
 			fileScanner.close();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -82,26 +118,33 @@ public class KontoauszugConverterToCSV implements Runnable {
 		}
 	}
 
-	public void processSets(Scanner fileScanner, String currentLine) {
-		while (!(currentLine.contains("Übertrag auf Blatt") || currentLine.contains("neuer Kontostand"))) {
-			if (currentLine.matches("\\d\\d.\\d\\d. \\d\\d.\\d\\d. .*")) {
-				String[] currentSet = new String[5];
+	public void processSets(Scanner fileScanner, String currentLine, String year) {
+		while (!isEndOfContentPage(currentLine)) {
+			if (isNewSet(currentLine)) {
 				currentLine = currentLine.trim().replaceAll("\\s+", " ");
-				currentSet[0] = currentLine.substring(0, 5);
-				currentSet[1] = currentLine.substring(7, 12);
-				currentSet[2] = currentLine.substring(14);
+				String bookingDay = currentLine.substring(0, 5) + "." + year;
+				String valueDay = currentLine.substring(7, 12) + "." + year;
+				String bookingData = currentLine.substring(14);
 				currentLine = trimNextLines(fileScanner);
-				currentSet[3] = currentLine;
-				currentLine = trimNextLines(fileScanner);
-				String mainLine = "";
-				while (!((currentLine.length() > 11 && currentLine.charAt(2) == '.' && currentLine.charAt(9) == '.')
-						|| currentLine.length() == 0 || currentLine.contains("Übertrag auf Blatt")
-						|| currentLine.contains("neuer Kontostand"))) {
-					mainLine += currentLine;
+				String sender = currentLine, information = "", ibanData = "";
+				if (!isNewSet(currentLine)) {
 					currentLine = trimNextLines(fileScanner);
+					String mainLine = "";
+					while (!(isNewSet(currentLine) || isEndOfContentPage(currentLine) || currentLine.length() == 0)) {
+						mainLine += currentLine;
+						currentLine = trimNextLines(fileScanner);
+					}
+					information = mainLine;
+					ibanData = "";
+					String[] mainParts = mainLine.split("IBAN:");
+					if (mainParts.length == 2
+							&& mainParts[1].matches("\\s?.{0,33}\\s?BIC:\\s?.{8,11}(\\s*ABWA:\\s?.*)?")) {
+						information = mainParts[0];
+						ibanData = "IBAN: " + mainParts[1];
+					}
 				}
-				currentSet[4] = mainLine;
-				results.add(currentSet);
+				String amount = bookingData.split("PN:\\d+")[1].trim().replaceAll("\\s", " ");
+				results.add(new String[] { bookingDay, valueDay, bookingData, sender, information, ibanData, amount });
 			} else {
 				int i = results.size();
 				results.get(i - 1)[4] = results.get(i - 1)[4] + currentLine;
@@ -115,13 +158,21 @@ public class KontoauszugConverterToCSV implements Runnable {
 				currentLine = fileScanner.nextLine().trim().replaceAll("\\s+", " ");
 			}
 			currentLine = trimNextLines(fileScanner);
-			processSets(fileScanner, currentLine);
-		} else if (currentLine.contains("neuer Kontostand")) {
-			return;
+			processSets(fileScanner, currentLine, year);
 		}
+		// end of file
 	}
-	
-	private String trimNextLines(Scanner fileScanner){
+
+	private boolean isEndOfContentPage(String currentLine) {
+		return currentLine.contains("Übertrag auf Blatt") || currentLine.contains("neuer Kontostand")
+				|| currentLine.contains("Bitte beachten Sie die Hinweise auf der Rückseite");
+	}
+
+	private boolean isNewSet(String currentLine) {
+		return currentLine.matches("\\d\\d.\\d\\d. \\d\\d.\\d\\d. .*");
+	}
+
+	private String trimNextLines(Scanner fileScanner) {
 		String currentLine = fileScanner.nextLine().trim().replaceAll("\\s+", " ");
 		while (currentLine.length() == 0) {
 			currentLine = fileScanner.nextLine().trim().replaceAll("\\s+", " ");
